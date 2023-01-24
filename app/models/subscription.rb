60a4ba3ebf4f39b36e140a6a6870da6aa073843f
Candace Eckels <candace.eckels@gmail.com>
2023-01-24 14:24:26 -0700

    Proved relationships for subscription model

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,3 +1,11 @@
 class Subscription < ApplicationRecord
   validates_presence_of :title, :price, :status, :frequency
+
+  has_many :customer_subscriptions
+  has_many :customers, through: :customer_subscriptions
+
+  has_many :subscription_teas
+  has_many :teas, through: :subscription_teas
+
+
 end
