c6923c27f4409373d2b6caacd5030af86e64852f
Candace Eckels <candace.eckels@gmail.com>
2023-01-24 13:54:22 -0700

    Added tests for tea model

--- spec/models/tea_spec.rb
+++ spec/models/tea_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Tea, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :description }
+    it { should validate_presence_of :temperature }
+    it { should validate_presence_of :brew_time }
+    it { should validate_presence_of :status }
+  end
+
+  describe 'relationships' do
+    xit { should have_many(:customer_subscriptions) }
+    xit { should have_many(:subscriptions).through(:customer_subscriptions) }
+  end
 end
