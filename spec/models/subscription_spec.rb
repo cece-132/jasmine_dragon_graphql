2a689e6d46fc97dc6991186e46c0a9f5f535b3e1
Candace Eckels <candace.eckels@gmail.com>
2023-01-24 13:49:22 -0700

    Added testing for subscription model

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Subscription, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :price }
+    it { should validate_presence_of :status }
+    it { should validate_presence_of :frequency }
+  end
+
+  describe 'relationships' do
+    xit { should have_many(:customer_subscriptions) }
+    xit { should have_many(:subscriptions).through(:customer_subscriptions) }
+  end
 end
