2e65f3f064515d25528c9af0461013bf1ae3149b
Candace Eckels <candace.eckels@gmail.com>
2023-01-24 14:35:49 -0700

    Proved relationships tea model

--- app/models/tea.rb
+++ app/models/tea.rb
@@ -1,3 +1,8 @@
 class Tea < ApplicationRecord
   validates_presence_of :title, :description, :temperature, :brew_time, :status
+
+   has_many :subscription_teas
+   has_many :subscriptions, through: :subscription_teas
+   has_many :customer_subscriptions, through: :subscriptions
+   has_many :customers, through: :customer_subscriptions
 end
--- spec/models/tea_spec.rb
+++ spec/models/tea_spec.rb
@@ -12,5 +12,7 @@ RSpec.describe Tea, type: :model do
   describe 'relationships' do
     it { should have_many(:subscription_teas) }
     it { should have_many(:subscriptions).through(:subscription_teas) }
+    it { should have_many(:customer_subscriptions).through(:subscriptions) }
+    it { should have_many(:customers).through(:customer_subscriptions) }
   end
 end
