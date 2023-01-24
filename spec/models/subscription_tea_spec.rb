5b8c231cef07f264c2e1f075f4c24a3397d786ea
Candace Eckels <candace.eckels@gmail.com>
2023-01-24 14:12:32 -0700

    Proved relationships for subscription_tea model

--- spec/models/subscription_tea_spec.rb
+++ spec/models/subscription_tea_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe SubscriptionTea, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of :tea_id }
+    it { should validate_presence_of :subscription_id }
+  end
+
+  describe 'relationships' do
+    it { should belong_to(:tea) }
+    it { should belong_to(:subscription) }
+  end
 end
