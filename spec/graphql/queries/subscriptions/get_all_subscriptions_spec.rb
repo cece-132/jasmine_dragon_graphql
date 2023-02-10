a76672335b8f718770c93903ede492e328a2fcab
Candace Eckels <candace.eckels@gmail.com>
2023-02-09 21:54:45 -0700

    Subscription test written

--- spec/graphql/queries/subscriptions/get_all_subscriptions_spec.rb
+++ spec/graphql/queries/subscriptions/get_all_subscriptions_spec.rb
@@ -7,7 +7,24 @@ RSpec.describe Types::QueryType do
     sub2 = Subscription.create!(title: "Black", price: 10000, status: "available", frequency: "General")
     sub3 = Subscription.create!(title: "Zen Master", price: 1500, status: "available", frequency: "Unique")
     sub4 = Subscription.create!(title: "Christmas Magic", price: 1300, status: "unavailable", frequency: "Seasonal")
-  end
+  
+    
+    describe "Display all subscriptions" do 
+      it 'should find all the subscriptions' do
+        result = JasmineDragonGraphqlSchema.execute(query).as_json
+        expect(result["data"]).to be_a Hash
+        expect(result["data"]).to have_key("subscriptions")
+        expect(result["data"]["subscriptions"]).to be_a Array
+        expect(result["data"]["subscriptions"].count).to eq 3
+  
+        result["data"]["subscriptions"].each do |subscription|
+          expect(subscription).to have_key("id")
+          expect(subscription).to have_key("title")
+          expect(subscription).to have_key("price")
+          expect(subscription).to have_key("frequency")
+        end
+      end
+    end
 
   def query
     <<~GQL
